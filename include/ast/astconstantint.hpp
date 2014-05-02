77619c4ad0b93eedd23980fb3a05f99aa212e80d
Jesse Mazis <recursiveowl@gmail.com>
2014-05-03 00:21:07 +1000

    ASTconstantint.toString () now correctly reports its value

--- include/ast/astconstantint.hpp
+++ include/ast/astconstantint.hpp
@@ -13,7 +13,7 @@ class ASTconstantInt : public ASTconstant <size_t> {
 	public:
 		const size_t & getValue () const {return val;}
 		virtual string getIden () const {return "";}
-		virtual string toString (const string & indent = "") const {return ASTconstant::toString (indent) + " 0";}
+		virtual string toString (const string & indent = "") const {std::stringstream ss; ss << ASTconstant::toString (indent) << val; return ss.str ();}
 		
 		ASTconstantInt () {}
 		ASTconstantInt (const ASTnode & type, const size_t val) : ASTconstant (type), val (val) {}
