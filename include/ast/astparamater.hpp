691df50301ffe7fdfaf77cc6e1998f0822c2ed2b
Jesse Mazis <recursiveowl@gmail.com>
2014-06-16 11:47:30 +1000

    Implemented getASTType ()

--- include/ast/astparamater.hpp
+++ include/ast/astparamater.hpp
@@ -14,6 +14,7 @@ class ASTparamater : public ASTbase {
 	VISITABLE
 	public:
 		const std::shared_ptr <NumbatVariable> & getVariable () {return variable;}
+		virtual const ASTnode getASTType () const {return variable->getASTType ();}
 		virtual bool isAlias () const {return variable->isAlias ();}
 		virtual bool isConst () const {return variable->isConst ();}
 		virtual bool isValid () const {return variable != nullptr;}
