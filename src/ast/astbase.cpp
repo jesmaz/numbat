cc688f634dd2644526a799036e587aecd57aee81
Jesse Mazis <recursiveowl@gmail.com>
2014-07-16 16:49:38 +1000

    Implemented static function getLength

--- src/ast/astbase.cpp
+++ src/ast/astbase.cpp
@@ -1,8 +1,20 @@
 #include "../include/ast/astbody.hpp"
+#include "../include/ast/aststructindex.hpp"
 
 namespace numbat {
 namespace parser {
 
+
+const ASTnode ASTbase::getLength (const ASTnode & exp) {
+	
+	ASTnode val = nullptr;
+	ssize_t index = exp->getLengthIndex ();
+	if (0 <= index) {
+		val = ASTnode (new ASTstructIndex (index, exp));
+	}
+	return val;
+	
+}
 
 ssize_t ASTbase::getLengthIndex () const {
 	
