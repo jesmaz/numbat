d0c5dccca41fad117845027d3950bf1a5e564307
Jesse Mazis <recursiveowl@gmail.com>
2014-08-26 16:58:46 +1000

    Implemented resolve symbol properly

--- include/ast/astbase.hpp
+++ include/ast/astbase.hpp
@@ -29,7 +29,8 @@ typedef shared_ptr <ASTbase> ASTnode;
 class ASTbase : public visitor::BaseVisitable {
 	public:
 		static const ASTnode getLength (const ASTnode & exp);
-		virtual ASTnode resolveSymbol (const string & iden) {return nullptr;}
+		static ASTnode resolveSymbol (const string & iden, const ASTnode & exp);
+		virtual ASTnode resolveSymbol (const string & iden) const;
 		virtual const ASTnode getArrayType () const;
 		virtual const ASTnode getASTType () const {return nullptr;}
 		virtual bool isAlias () const {return false;}
@@ -48,6 +49,31 @@ class ASTbase : public visitor::BaseVisitable {
 		virtual string toString (const string & indent = "") const=0;
 		virtual ssize_t getLengthIndex () const;
 		ASTbase () {}
+};
+
+class ASTwrapper : public ASTbase {
+	public:
+		virtual ASTnode resolveSymbol (const string & iden) const {return node->resolveSymbol (iden);}
+		virtual const ASTnode getArrayType () const {return node->getArrayType ();}
+		virtual const ASTnode getASTType () const {return node->getASTType();}
+		virtual bool isAlias () const {return node->isAlias ();}
+		virtual bool isArray () const {return node->isArray ();}
+		virtual bool isCallable () const {return node->isCallable ();}
+		virtual bool isConst () const {return node->isConst ();}
+		virtual bool isList () const {return node->isList ();}
+		virtual bool isNil () const {return node->isNil ();}
+		virtual bool isRaw () const {return node->isRaw ();}
+		virtual bool isValid () const {return node->isValid ();}
+		virtual shared_ptr <NumbatType> getType () const {return node->getType ();}
+		virtual size_t calculateWeight () const {return node->calculateWeight ();}
+		virtual size_t getBitSize () const {return node->getBitSize ();}
+		virtual string getIden () const {return node->getIden ();}
+		virtual string toString (const string & indent = "") const {return node->toString (indent);}
+		virtual ssize_t getLengthIndex () const {return node->getLengthIndex ();}
+		virtual void accept (visitor::BaseVisitor & vis) {const_cast <ASTbase *> (node)->accept (vis);}
+		ASTwrapper (const ASTbase * node) : node (node) {}
+	private:
+		const ASTbase * node;
 };
 
 class ASTlistInterface {
