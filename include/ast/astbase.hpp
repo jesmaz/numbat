23493e188b75d6ca41da6cdf1b4c7a52f4a36414
Jesse Mazis <recursiveowl@gmail.com>
2014-08-27 10:46:23 +1000

    Removes ASTwrapper, it was accidentally committed

--- include/ast/astbase.hpp
+++ include/ast/astbase.hpp
@@ -51,31 +51,6 @@ class ASTbase : public visitor::BaseVisitable {
 		ASTbase () {}
 };
 
-class ASTwrapper : public ASTbase {
-	public:
-		virtual ASTnode resolveSymbol (const string & iden) const {return node->resolveSymbol (iden);}
-		virtual const ASTnode getArrayType () const {return node->getArrayType ();}
-		virtual const ASTnode getASTType () const {return node->getASTType();}
-		virtual bool isAlias () const {return node->isAlias ();}
-		virtual bool isArray () const {return node->isArray ();}
-		virtual bool isCallable () const {return node->isCallable ();}
-		virtual bool isConst () const {return node->isConst ();}
-		virtual bool isList () const {return node->isList ();}
-		virtual bool isNil () const {return node->isNil ();}
-		virtual bool isRaw () const {return node->isRaw ();}
-		virtual bool isValid () const {return node->isValid ();}
-		virtual shared_ptr <NumbatType> getType () const {return node->getType ();}
-		virtual size_t calculateWeight () const {return node->calculateWeight ();}
-		virtual size_t getBitSize () const {return node->getBitSize ();}
-		virtual string getIden () const {return node->getIden ();}
-		virtual string toString (const string & indent = "") const {return node->toString (indent);}
-		virtual ssize_t getLengthIndex () const {return node->getLengthIndex ();}
-		virtual void accept (visitor::BaseVisitor & vis) {const_cast <ASTbase *> (node)->accept (vis);}
-		ASTwrapper (const ASTbase * node) : node (node) {}
-	private:
-		const ASTbase * node;
-};
-
 class ASTlistInterface {
 	public:
 		virtual const std::vector <ASTnode> getElements () const=0;
