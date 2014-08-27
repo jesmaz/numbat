cc7fa3cf72f06ce905b558a5364ec97a95da0fd3
Jesse Mazis <recursiveowl@gmail.com>
2014-08-27 20:33:52 +1000

    Added a context struct

--- include/parser.hpp
+++ include/parser.hpp
@@ -27,6 +27,20 @@ struct Position {
 	Position (tkitt itt, tkitt end) : itt (itt), end (end) {allign ();}
 };
 
+struct ParsingContext {
+	string path, file;
+	
+	std::map <string, shared_ptr <OperatorDecleration>> operators;
+	
+	std::multimap <string, shared_ptr <OperatorDecleration>> operatorsByFirstToken;
+	
+	std::set <shared_ptr <OperatorDecleration>, std::greater <shared_ptr <OperatorDecleration>>> precidenceOrderedOperators;
+	
+	std::unordered_map <string, ASTnode(*)(AbstractSyntaxTree *, tkitt)> statementParsers;
+	
+	std::unordered_set <string> parenOpperators, oppTokens, ternaryStart;
+};
+
 void parseBody (Parser parser, NumbatScope * body);
 
 
