a4d3790d3ec1bd0ea9d65e1790044ef5439a40a3
Jesse Mazis <recursiveowl@gmail.com>
2014-09-03 20:31:26 +1000

    Moved Position into its own file

--- include/parser.hpp
+++ include/parser.hpp
@@ -2,30 +2,11 @@
 #define PARSER_HPP
 
 #include "ast.hpp"
+#include "position.hpp"
 
 namespace numbat {
 namespace parser {
 
-
-struct Position {
-	tkitt itt, end;
-	operator bool () const {return itt < end;}
-	friend const bool operator < (const Position & lhs, const Position & rhs) {return lhs.itt < rhs.itt;}
-	friend const bool operator > (const Position & lhs, const Position & rhs) {return lhs.itt > rhs.itt;}
-	friend const bool operator <= (const Position & lhs, const Position & rhs) {return lhs.itt <= rhs.itt;}
-	friend const bool operator >= (const Position & lhs, const Position & rhs) {return lhs.itt >= rhs.itt;}
-	friend const bool operator != (const Position & lhs, const Position & rhs) {return lhs.itt != rhs.itt;}
-	friend const bool operator == (const Position & lhs, const Position & rhs) {return lhs.itt == rhs.itt;}
-	const Position & allign () {while ((itt->type == lexer::TOKEN::indent or itt->type == lexer::TOKEN::whitespace) and itt<end) ++itt; return *this;}
-	template <typename T>
-	const Position operator + (T num) const {return Position (itt + num, end).allign ();}
-	const Position operator + (const Position & rhs) const {return Position (rhs.end + 1, end).allign ();}
-	const Position operator ++ () {return Position (itt ++, end).allign ();}
-	const Position & operator ++ (int) {++itt; return this->allign ();}
-	const Position & operator += (const Position & rhs) {return *this = Position (rhs.end + 1, end).allign ();}
-	Position (tkitt itt) : Position (itt, end) {}
-	Position (tkitt itt, tkitt end) : itt (itt), end (end) {allign ();}
-};
 
 struct ParsingContext {
 	string path, file;
