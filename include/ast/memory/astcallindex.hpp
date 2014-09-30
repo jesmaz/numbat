9898a91957a3aca88adc0a9d954e77ff3e2a1910
Jesse Mazis <recursiveowl@gmail.com>
2014-09-30 12:20:11 +1000

    Calls can now be indexed with a raw pointer

--- include/ast/memory/astcallindex.hpp
+++ include/ast/memory/astcallindex.hpp
@@ -12,23 +12,26 @@ namespace parser {
 class ASTcallindex : public ASTbase {
 	VISITABLE
 	public:
-		const shared_ptr <ASTcallable> & getCall () const {return call;}
+		ASTcallable * getCall () {return callp ? callp : call.get ();}
+		const ASTcallable * getCall () const {return callp ? callp : call.get ();}
 		const size_t getIndex () const {return index;}
 		virtual const ASTnode getASTType () const {return ref->getASTType ();}
 		virtual bool isAlias () const {return ref->isAlias ();}
 		virtual bool isConst () const {return ref->isConst ();}
-		virtual bool isValid () const {return call->isValid ();}
+		virtual bool isValid () const {return (callp ? callp : call.get ())->isValid ();}
 		virtual const NumbatType * getType () const {return ref->getType ();}
-		virtual size_t calculateWeight () const {return call->calculateWeight ();}
+		virtual size_t calculateWeight () const {return (callp ? callp : call.get ())->calculateWeight ();}
 		virtual size_t getBitSize () const {return ref->getBitSize ();}
 		virtual string getIden () const {return ref->getIden ();}
-		virtual string toString (const string & indent = "") const {return !index ? call->toString (indent) : ref->toString (indent);}
+		virtual string toString (const string & indent = "") const {return (callp ? callp : call.get ())->toString (indent) + " " + ref->toString ();}
 		
 		ASTcallindex () {}
 		ASTcallindex (const shared_ptr <ASTcallable> & call, const size_t index) : ref (call->getFunction ()->getType () [index]), call (call), index (index) {}
+		ASTcallindex (ASTcallable * call, const size_t index) : ref (call->getFunction ()->getType () [index]), callp (call), index (index) {}
 	private:
 		ASTnode ref;
 		shared_ptr <ASTcallable> call;
+		ASTcallable * callp;
 		size_t index;
 };
 
