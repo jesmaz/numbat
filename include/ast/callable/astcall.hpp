81516d497bc611ac81bce772f84dd15ea9c071a2
Jesse Mazis <recursiveowl@gmail.com>
2014-09-30 22:24:21 +1000

    Made ASTcallable a list node

--- include/ast/callable/astcall.hpp
+++ include/ast/callable/astcall.hpp
@@ -13,11 +13,13 @@ class ASTcall : public ASTcallable {
 	public:
 		const shared_ptr <ASTcallable> & getCallee () const {return callee;}
 		const std::vector <ASTnode> & getArgs () const {return args;}
+		virtual bool isList () const {return true;}
 		virtual bool isParsed () const;
 		virtual bool isValid () const;
 		virtual const FunctionDecleration * getFunction () const {return callee->getFunction ();}
 		virtual size_t calculateWeight () const;
 		virtual size_t getBitSize () const {return 0;}
+		virtual const std::list <ASTnode> getList () const;
 		virtual string getIden () const {return callee->getIden ();}
 		virtual string toString (const string & indent = "") const;
 		
