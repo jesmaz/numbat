4d51282046c932557261577a976d93619f688324
Jesse Mazis <recursiveowl@gmail.com>
2015-06-19 11:34:47 +1000

    Added struct data type for the parse tree

---
+++ include/parse/tree/struct.hpp
@@ -0,0 +1,27 @@
+#ifndef STRUCT_HPP
+#define STRUCT_HPP
+
+
+#include "base.hpp"
+
+#include <vector>
+
+
+struct Struct : ParseTreeNode {
+	
+	public:
+		
+		Struct (uint32_t line, uint32_t pos) : ParseTreeNode (line, pos) {}
+		Struct (const std::vector <PTNode> & args) : ParseTreeNode (args.front ()->getLine (), args.front ()->getPos ()) {}
+		
+	protected:
+	private:
+		
+		virtual string strDump () {return "";}
+		
+		std::vector <PTNode> tplate, tags, type;
+		
+};
+
+
+#endif /*STRUCT_HPP*/
