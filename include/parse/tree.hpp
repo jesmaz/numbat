b17fc5d7e332128d25aa1791f5803457c1c420a5
Jesse Mazis <recursiveowl@gmail.com>
2015-06-19 11:33:54 +1000

    Added getters. Fixed crash when printing an empty tree.

--- include/parse/tree.hpp
+++ include/parse/tree.hpp
@@ -3,6 +3,7 @@
 
 
 #include "tree/function.hpp"
+#include "tree/struct.hpp"
 
 #include <vector>
 
@@ -11,16 +12,21 @@ class ParseTree : public ParseTreeNode {
 	
 	public:
 		
+		const std::vector <Function *> & getFunctions () const {return functions;}
+		const std::vector <PTNode> & getBody () const {return body;}
+		const std::vector <Struct *> & getStructs () const {return structs;}
+		
 		ParseTree (uint32_t line, uint32_t pos) : ParseTreeNode (line, pos) {}
 		ParseTree (const std::vector <PTNode> & body) : ParseTreeNode (body.front ()->getLine (), body.front ()->getPos ()), body (body) {}
 		
 	protected:
 	private:
 		
-		virtual string strDump () {string s = "{"; for (size_t i=0; i+1<body.size (); ++i) s += body [i]->toString () + "; "; return s + body.back ()->toString () + ";}";}
+		virtual string strDump () {if (body.size ()) {string s = "{"; for (size_t i=0; i+1<body.size (); ++i) s += body [i]->toString () + "; "; return s + body.back ()->toString () + ";}";} else {return "{}";}}
 		
 		std::vector <PTNode> body;
 		std::vector <Function *> functions;
+		std::vector <Struct *> structs;
 		
 };
 
