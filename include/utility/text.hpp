f681ebd6a2660cd752f8139a9d16bfa34b4b930d
Jesse Mazis <recursiveowl@gmail.com>
2015-06-21 21:53:53 +1000

    Added generic functions to print lists of values

--- include/utility/text.hpp
+++ include/utility/text.hpp
@@ -13,15 +13,31 @@ enum PrintMode {PLAIN=0, COLOUR=1};
 const std::string normal="\x1B[0m",
 	red="\x1B[31m",
 	grn="\x1B[32m",
-	blu="\x1B[33m",
-	yel="\x1B[34m",
+	yel="\x1B[33m",
+	blu="\x1B[34m",
 	mag="\x1B[35m",
 	cyn="\x1B[36m",
 	wht="\x1B[37m",
 	reset="\033[0m";
 
-};
 
+template <typename ForwardItt>
+std::string listPtrToStr (ForwardItt beg, ForwardItt end) {
+	if (beg == end) return "";
+	std::string s = (*beg)->toString ();
+	while (++beg != end) s += ", " + (*beg)->toString ();
+	return s;
+}
+
+template <typename ForwardItt, typename ...Args>
+std::string listPtrToStr (ForwardItt beg, ForwardItt end, Args ... args) {
+	if (beg == end) return "";
+	std::string s = (*beg)->toString (args...);
+	while (++beg != end) s += ", " + (*beg)->toString (args...);
+	return s;
+}
+
+};
 
 
 #endif /*TEXTUTIL_HPP*/
