365ea6af56b016fb8da4b1057d03274707d5fe1a
Jesse Mazis <recursiveowl@gmail.com>
2015-06-22 21:34:53 +1000

    Implemented strDump for Function

--- src/parse/tree/function.cpp
+++ src/parse/tree/function.cpp
@@ -1,5 +1,15 @@
 #include "../../../include/parse/tree/function.hpp"
 
 string Function::strDump (text::PrintMode mode) {
-	return "";
+	
+	string s = iden.empty () ? "" : "def ";
+	s = mode&text::COLOUR ? text::blu + s + text::reset : s;
+	s += iden.empty () ? "" : iden + " ";
+	if (not tplate.empty ())
+		s += "@[" + text::listPtrToStr (tplate.begin (), tplate.end (), mode) + "]";
+	s += "(" + text::listPtrToStr (params.begin (), params.end (), mode) + ")";
+	s += "->(" + text::listPtrToStr (type.begin (), type.end (), mode) + ")";
+	s += body->toString ();
+	return s;
+	
 }
