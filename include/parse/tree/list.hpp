897bf99130d6f48d39f6c46ae32d55aefb574a9a
Jesse Mazis <recursiveowl@gmail.com>
2015-06-26 22:28:39 +1000

    Added a list node

---
+++ include/parse/tree/list.hpp
@@ -0,0 +1,30 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+
+#include "base.hpp"
+
+
+class ParseTreeList : public ParseTreeNode {
+	
+	public:
+		
+		virtual bool isAggregate () {return true;}
+		virtual const std::vector <ParseTreeNode *> & getArgs () const {return args;}
+		virtual const std::vector <ParseTreeNode *> & releaseArgs () const {auto a = args; args = std::vector <PTNode> (); return a;}
+		
+		ParseTreeList (uint32_t line, uint32_t pos) : ParseTreeNode (line, pos) {}
+		ParseTreeList (const std::vector <PTNode> & args) : ParseTreeNode (args.front ()->getLine (), args.front ()->getPos ()), args (args) {}
+		virtual ~ParseTreeList () {for (PTNode n : args) delete n;}
+		
+	protected:
+	private:
+		
+		virtual string strDump (text::PrintMode mode);
+		
+		std::vector <PTNode> args;
+		
+};
+
+
+#endif /*LIST_HPP*/
