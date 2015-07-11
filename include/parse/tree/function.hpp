f8fd406bcf4893f15f45e9c5278b482ee7885df3
Jesse Mazis <recursiveowl@gmail.com>
2015-07-11 12:28:28 +1000

    Implemented virtual functions

--- include/parse/tree/function.hpp
+++ include/parse/tree/function.hpp
@@ -11,8 +11,11 @@ struct Function : ParseTreeNode {
 	
 	public:
 		
+		const std::vector <PTNode> & getParams () const {return params;}
+		const std::vector <PTNode> & getType () const {return type;}
 		
 		virtual Function * asFunction () {return this;}
+		virtual const string & getIden () const {return iden;}
 		
 		Function (uint32_t line, uint32_t pos) : ParseTreeNode (ParseTreeNode::NodeType::FUNCTION, line, pos), body (nullptr) {}
 		Function (const std::vector <PTNode> & args);
