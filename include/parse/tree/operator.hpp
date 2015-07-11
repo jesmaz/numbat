152a1ff29523264d4a6cc6bf702e2a54531a2e99
Jesse Mazis <recursiveowl@gmail.com>
2015-07-11 12:27:18 +1000

    Implemented the virtual getArgs metho

--- include/parse/tree/operator.hpp
+++ include/parse/tree/operator.hpp
@@ -12,6 +12,7 @@ class ParseTreeOperator : public ParseTreeNode {
 	public:
 		
 		virtual bool isAggregate () {return true;}
+		virtual const std::vector <ParseTreeNode *> & getArgs () const {return args;}
 		
 		ParseTreeOperator (uint32_t line, uint32_t pos) : ParseTreeNode (line, pos) {}
 		ParseTreeOperator (const std::vector <PTNode> & args) : ParseTreeNode (args.front ()->getLine (), args.front ()->getPos ()), args (args) {}
