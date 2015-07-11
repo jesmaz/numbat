76de22d380e299f2a4a4b813340aa8c0bc288015
Jesse Mazis <recursiveowl@gmail.com>
2015-07-11 12:26:42 +1000

    Symbol not has the SYMBOL type

--- include/parse/tree/symbol.hpp
+++ include/parse/tree/symbol.hpp
@@ -9,7 +9,7 @@ class ParseTreeSymbol : public ParseTreeNode {
 	
 	public:
 		
-		ParseTreeSymbol (uint32_t line, uint32_t pos, string sym) : ParseTreeNode (line, pos), sym (sym) {}
+		ParseTreeSymbol (uint32_t line, uint32_t pos, string sym) : ParseTreeNode (ParseTreeNode::NodeType::SYMBOL, line, pos), sym (sym) {}
 		
 	protected:
 	private:
