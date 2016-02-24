968cfb61a1f6928b11e955a64801e56caabc5def
Jesse Mazis <recursiveowl@gmail.com>
2016-02-25 01:48:09 +1100

    Added background colours & black text colour

--- include/utility/text.hpp
+++ include/utility/text.hpp
@@ -11,6 +11,7 @@ namespace text {
 enum PrintMode {PLAIN=0, COLOUR=1};
 
 const std::string normal="\x1B[0m",
+	black="\x1B[30m",
 	red="\x1B[31m",
 	grn="\x1B[32m",
 	yel="\x1B[33m",
@@ -18,6 +19,15 @@ const std::string normal="\x1B[0m",
 	mag="\x1B[35m",
 	cyn="\x1B[36m",
 	wht="\x1B[37m",
+	def="\x1B[39m",
+	bgRed="\x1B[41m",
+	bgGrn="\x1B[42m",
+	bgYel="\x1B[43m",
+	bgBlu="\x1B[44m",
+	bgMag="\x1B[45m",
+	bgCyn="\x1B[46m",
+	bgWht="\x1B[47m",
+	bgDef="\x1B[49m",
 	reset="\033[0m";
 
 
