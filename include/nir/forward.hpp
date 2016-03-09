f0788d9126fd3e2a67adb01d4448820928ca2843
Jesse Mazis <recursiveowl@gmail.com>
2016-03-09 16:45:29 +1100

    Added forwad declerations for nir

---
+++ include/nir/forward.hpp
@@ -0,0 +1,25 @@
+#ifndef NIR_FORWARD_HPP
+#define NIR_FORWARD_HPP
+
+
+namespace nir {
+
+
+class Function;
+class Instruction;
+class Number;
+class Parameter;
+class PointerType;
+class Struct;
+class Type;
+
+struct Block;
+struct Builder;
+struct Module;
+struct Scope;
+
+
+};
+
+
+#endif/*NIR_FORWARD_HPP*/
