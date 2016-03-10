8a4a0271bce61e88f143a1e2ec06cffeda06c644
Jesse Mazis <recursiveowl@gmail.com>
2016-03-10 21:36:24 +1100

    Addded nir instruction

---
+++ include/nir/instruction.hpp
@@ -0,0 +1,44 @@
+#ifndef NIR_INSTRUCTION
+#define NIR_INSTRUCTION
+
+
+#include "../visitor.hpp"
+#include "type.hpp"
+
+#include <cassert>
+
+
+namespace nir {
+
+using std::string;
+
+typedef const string * symbol;
+
+class Instruction : public numbat::visitor::BaseConstVisitable {
+	
+	public:
+		
+		virtual bool validate () const=0;
+		string printIden (text::PrintMode mode=text::PLAIN) const;
+		string toString (text::PrintMode mode=text::PLAIN) const;
+		
+		const Type * const getType () const {return type;}
+		symbol getIden () const {return iden;}
+		
+	protected:
+		
+		Instruction (const Type * type, symbol iden) : type (type), iden (iden) {}
+		
+	private:
+		
+		virtual string strDump (text::PrintMode mode) const=0;
+		
+		const Type * type;
+		symbol iden;
+		
+};
+
+};
+
+
+#endif/*NIR_TYPE_BASE_HPP*/
