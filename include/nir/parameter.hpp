5e1471ee3a960d1392ff1090766b3b2c51df6080
Jesse Mazis <recursiveowl@gmail.com>
2016-03-11 20:28:19 +1100

    Added param instruction

---
+++ include/nir/parameter.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "forward.hpp"
+#include "instruction.hpp"
+
+
+namespace nir {
+
+
+class Parameter : public Instruction {
+	CONST_VISITABLE
+	public:
+		
+		bool validate () const {return true;}
+		
+		const Instruction * getDefVal () const {return defVal;}
+		
+		Parameter (const Instruction * instruction, const string & iden) : Instruction (instruction->getType (), &(this->iden)), defVal (instruction), iden (iden) {}
+		Parameter (const Type * type, const string & iden) : Instruction (type, &(this->iden)), iden (iden) {}
+		
+	protected:
+	private:
+		
+		string strDump (text::PrintMode mode) const {return "NYI Parameter.strDump";}
+		
+		const Instruction * defVal;
+		string iden;
+		
+};
+
+
+};
