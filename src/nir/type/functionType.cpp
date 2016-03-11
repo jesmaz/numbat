4e00f9fc150c3669785e5344c32023782a2ebb11
Jesse Mazis <recursiveowl@gmail.com>
2016-03-11 20:34:34 +1100

    Implemented function and pointer types

---
+++ src/nir/type/functionType.cpp
@@ -0,0 +1,9 @@
+#include "../../../include/nir/type/functionType.hpp"
+
+namespace nir {
+
+
+std::map <string, FunctionType *> FunctionType::functionTypes;
+
+
+};
---
+++ src/nir/type/pointer.cpp
@@ -0,0 +1,9 @@
+#include "../../../include/nir/type/pointer.hpp"
+
+namespace nir {
+
+
+std::map <const Type *, PointerType *> PointerType::pointerTypes;
+
+
+};
