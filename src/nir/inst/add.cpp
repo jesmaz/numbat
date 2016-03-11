0f187b74bd0674b426ee3359c54941f2c615f480
Jesse Mazis <recursiveowl@gmail.com>
2016-03-11 20:34:04 +1100

    Implemented basic arithmatic instructions

---
+++ src/nir/inst/add.cpp
@@ -0,0 +1,34 @@
+#include "../../../include/nir/inst/add.hpp"
+
+
+namespace nir {
+
+
+bool Add::validate () const {
+	
+	/*assert (lhs);
+	assert (rhs);
+	auto * lhsType = lhs->getType ();
+	auto * rhsType = rhs->getType ();
+	assert (lhsType);
+	assert (rhsType);
+	assert (lhsType->isNumber ());
+	assert (rhsType->isNumber ());
+	if (lhsType != rhsType) {
+		assert (width, "Bit width must be specified if lhs and rhs are not the same size");
+		if (lhsType->numberType () != lhsType->numberType ()) {
+			assert (type, "Number type must be defined if lhs and rhs are not the same type");
+		}
+	}*/
+	return true;
+	
+}
+
+string Add::strDump (text::PrintMode mode) const {
+	
+	return "add." + getType ()->toString (mode) + " " + lhs->printIden (mode) + " " + rhs->printIden (mode);
+	
+}
+
+
+};
---
+++ src/nir/inst/sub.cpp
@@ -0,0 +1,34 @@
+#include "../../../include/nir/inst/sub.hpp"
+
+
+namespace nir {
+
+
+bool Sub::validate () const {
+	
+	/*assert (lhs);
+	assert (rhs);
+	auto * lhsType = lhs->getType ();
+	auto * rhsType = rhs->getType ();
+	assert (lhsType);
+	assert (rhsType);
+	assert (lhsType->isNumber ());
+	assert (rhsType->isNumber ());
+	if (lhsType != rhsType) {
+		assert (width, "Bit width must be specified if lhs and rhs are not the same size");
+		if (lhsType->numberType () != lhsType->numberType ()) {
+			assert (type, "Number type must be defined if lhs and rhs are not the same type");
+		}
+	}*/
+	return true;
+	
+}
+
+string Sub::strDump (text::PrintMode mode) const {
+	
+	return "sub." + getType ()->toString (mode) + " " + lhs->printIden (mode) + " " + rhs->printIden (mode);
+	
+}
+
+
+};
