defbe1fec6fe2022c06a477dd730f47ce5a05af9
Jesse Mazis <recursiveowl@gmail.com>
2016-03-11 20:25:03 +1100

    Added virtual base targt

---
+++ include/codegen/target.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+
+#include "../nir/forward.hpp"
+
+#include <cinttypes>
+#include <map>
+#include <string>
+#include <vector>
+
+
+namespace codegen {
+
+using std::string;
+using namespace nir;
+
+class Target {
+	
+	public:
+		
+		virtual void reset ()=0;
+		virtual void visit (const Function * module)=0;
+		virtual void visit (const Type * module)=0;
+		
+		void * jitFunction (const nir::Function *);
+		
+		static Target * find (const string & iden);
+		
+		virtual ~Target () {}
+		
+	protected:
+		
+		typedef Target* (*FFunc)();
+		struct RegTarget {
+			RegTarget (const string & iden, FFunc ffunc);
+		};
+		
+	private:
+		
+		virtual void generate (const nir::Function * func, std::vector <uint8_t> & output)=0;
+		
+		static std::map <string, FFunc> * targets;
+		
+};
+
+
+};
