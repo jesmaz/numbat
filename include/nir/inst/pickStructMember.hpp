043783ec4a0a307573294ed7f407d76a3a521d3b
Jesse Mazis <recursiveowl@gmail.com>
2016-07-21 15:48:13 +1000

    Added instruction to isolate struct members

---
+++ include/nir/inst/pickStructMember.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <nir/instruction.hpp>
+
+
+namespace nir {
+
+
+class PickStructMember : public Instruction {
+	CONST_VISITABLE
+	public:
+		
+		bool validate () const {return true;}
+		
+		Argument getParent () const {return parent;}
+		
+	protected:
+	private:
+		
+		virtual string strDump (text::PrintMode mode) const {return parent.toString (mode) + "." + memIden;}
+		
+		PickStructMember (const Type * type, Argument parent, size_t index, const string & memIden, symbol iden) : Instruction ({type}, {iden}), parent (parent), index (index), memIden (memIden) {}
+		
+		Argument parent;
+		size_t index;
+		string memIden;
+		
+		friend Scope;
+		
+};
+
+
+};
