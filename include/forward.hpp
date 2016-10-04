bbb0900cf12a39238bc44e78363d6b5055e37e6e
Jesse Mazis <recursiveowl@gmail.com>
2016-10-04 19:24:25 +1100

    Added forward declerations file

---
+++ include/forward.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include <string>
+
+using std::string;
+
+
+namespace numbat {
+
+
+struct File;
+
+
+}
+
+
+namespace nir {
+
+
+struct Module;
+struct Scope;
+
+
+}
+
+
+namespace parser {
+
+
+using std::string;
+
+
+class ParseTreeNode;
+struct Function;
+struct Struct;
+
+typedef ParseTreeNode * PTNode;
+
+
+}
