67b81d3942ee2e9d30c3dff0e47fbd875a88505e
Jesse Mazis <recursiveowl@gmail.com>
2017-05-22 11:35:37 +1000

    Added AST context

---
+++ include/ast/context.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+
+#include <ast/function.hpp>
+#include <ast/node.hpp>
+#include <ast/type.hpp>
+#include <forward.hpp>
+#include <map>
+
+
+namespace AST {
+
+
+enum class FUNC_MATCH {EXACT, IMPLICIT, POLYMORPHIC, MIS_MATCH};
+
+
+FUNC_MATCH rateMatch (const Function & func, const BasicArray <TypePtr> & args);
+FUNC_MATCH rateMatch (const Function & func, const BasicArray <TypePtr> & args, const std::map <string, TypePtr> & params);
+
+
+struct Context {
+	
+	public:
+		
+		struct function_t {
+			std::map <FuncPtr, const Context *> funcs;
+			const Context * owner;
+		};
+		
+		struct type_t {
+			TypePtr type;
+			const Context * owner;
+		};
+		
+		struct variable_t {
+			NodePtr var;
+			const Context * owner;
+		};
+		
+		const numbat::File * getSourceFile () const {return sourceFile;}
+		
+		Context * getRootContext () const {return rootContex;}
+		
+		NodePtr resolve (const string & str) const;
+		TypePtr resolveType (const string & str) const;
+		
+		void func (const string & str, FuncPtr func);
+		void import (const string & str, Context * ctx);
+		void type (const string & str, TypePtr t);
+		void var (const string & str, NodePtr v);
+		
+		std::shared_ptr <function_t> reserveFunc (const string & str);
+		std::shared_ptr <type_t> reserveType (const string & str);
+		
+		Context (const numbat::File * sourceFile) : imports (new std::map <string, Context *> ()),
+		             functions (new std::map <string, std::shared_ptr <function_t>> ()),
+		             types (new std::map <string, std::shared_ptr <type_t>> ()),
+		             variables (new std::map <string, std::shared_ptr <variable_t>> ()),
+		             sourceFile (sourceFile),
+		             ownImp (true),
+		             ownFunc (true),
+		             ownType (true),
+		             ownVar (true),
+		             rootContex (this) {}
+		             
+		Context (const Context & other) : imports (other.imports),
+		             functions (other.functions),
+		             types (other.types),
+		             variables (other.variables),
+		             sourceFile (other.sourceFile),
+		             ownImp (false),
+		             ownFunc (false),
+		             ownType (false),
+		             ownVar (false),
+		             rootContex (this) {}
+		             
+		Context (const Context & other, const numbat::File * source) : Context (other) {sourceFile = source;}
+		
+	protected:
+	private:
+		
+		const Context & operator = (const Context &)=delete;
+		
+		std::shared_ptr <std::map <string, Context *>> imports;
+		std::shared_ptr <std::map <string, std::shared_ptr <function_t>>> functions;
+		std::shared_ptr <std::map <string, std::shared_ptr <type_t>>> types;
+		std::shared_ptr <std::map <string, std::shared_ptr <variable_t>>> variables;
+		const numbat::File * sourceFile=nullptr;
+		bool ownImp, ownFunc, ownType, ownVar;
+		
+		Context * rootContex=nullptr;
+		
+};
+
+class Function_Set : public Node {
+	
+	public:
+		
+		const Context::function_t & getSet () const {return *set;}
+		
+		void accept (AbstractPass & pass) const {pass.visit (*this);}
+		virtual string toString (text::PrintMode mode) const;
+		
+		Function_Set (numbat::lexer::position pos, const std::shared_ptr <Context::function_t> & set) : Node (pos), set (set) {}
+		
+	protected:
+	private:
+		
+		std::shared_ptr <Context::function_t> set;
+		
+};
+
+}
---
+++ src/ast/context.cpp
@@ -0,0 +1,140 @@
+#include <ast/context.hpp>
+#include <ast/function.hpp>
+#include <iostream>
+#include <utility/report.hpp>
+
+
+namespace AST {
+
+
+NodePtr Context::resolve (const string & str) const {
+	
+	auto itt = variables->find (str);
+	if (itt != variables->end ()) {
+		return itt->second->var;
+	}
+	std::cerr << str << " not found " << std::hex << (void*)this << std::endl;
+	return nullptr;
+	
+}
+
+TypePtr Context::resolveType (const string & str) const {
+	
+	auto itt = types->find (str);
+	if (itt != types->end ()) {
+		return itt->second->type;
+	}
+	return nullptr;
+	
+}
+
+
+void Context::func (const string & str, FuncPtr func) {
+	
+	std::cerr << "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ " << str << std::endl;
+	
+	if (not ownFunc) {
+		functions = std::make_shared <std::map <string, std::shared_ptr <function_t>>> (*functions);
+		ownFunc = true;
+	}
+	
+	if (not ownVar) {
+		variables = std::make_shared <std::map <string, std::shared_ptr <variable_t>>> (*variables);
+		ownVar = true;
+	}
+	
+	auto ptr = (*functions) [str];
+	if (not ptr) {
+		ptr = std::make_shared <function_t> (function_t {{}, this});
+		
+	} else if (ptr->owner != this) {
+		ptr = std::make_shared <function_t> (function_t {ptr->funcs, this});
+		
+	}
+	
+	auto var = variables->find (str);
+	if (var == variables->end () or var->second->owner != this) {
+		auto f = std::make_shared <variable_t> (variable_t {
+			std::make_shared <AST::Function_Set> (func->getPos (), ptr),
+			this
+		});
+		this->var (str, std::make_shared <AST::Function_Set> (func->getPos (), ptr));
+		std::cerr << "RRRRRRRRRRRRRR " << str << " " << std::hex << (void*)this << std::endl;
+		
+	}
+	
+	auto & map = ptr->funcs;
+	auto itt = map.find (func);
+	if (itt == map.end ()) {
+		map [func] = this;
+		
+	} else if (itt->second == this) {
+		report::logMessage (
+			report::ERROR,
+			sourceFile,
+			func->getPos (),
+			"Identical definitions of '" + str + "'. First defined here: "//TODO: print old position
+		);
+		
+	} else {
+		map.erase (func);
+		map [func] = this;
+		
+	}
+	
+	if (not func->getParams ().empty ()) {
+		func->getParams () [0]->overloadFunc (str, func);
+	}
+	
+}
+
+void Context::import (const string & str, Context * ctx) {
+	
+	if (not ownImp) {
+		imports = std::make_shared <std::map <string, Context *>> (*imports);
+		ownImp = true;
+	}
+	
+}
+
+void Context::type (const string & str, TypePtr t) {
+	
+	if (not ownType) {
+		types = std::make_shared <std::map <string, std::shared_ptr <type_t>>> (*types);
+		ownType = true;
+	}
+	
+}
+
+void Context::var (const string & str, NodePtr v) {
+	
+	if (not ownVar) {
+		variables = std::make_shared <std::map <string, std::shared_ptr <variable_t>>> (*variables);
+		ownVar = true;
+	}
+	
+	auto itt = variables->find (str);
+	if (itt == variables->end () or itt->second->owner != this) {
+		std::cerr << "Registered '" << str << "'" << std::endl;
+		(*variables) [str] = std::make_shared <variable_t> (variable_t {v, this});
+		
+	} else {
+		report::logMessage (
+			report::ERROR,
+			sourceFile,
+			v->getPos (),
+			"Duplicate variable '" + str + "'. First defined here: "//TODO: print old position
+		);
+		std::cerr << ("Duplicate variable '" + str + "'. First defined here: ") << std::endl;
+		
+	}
+	
+}
+
+string Function_Set::toString (text::PrintMode mode) const {
+	
+	return getSet ().funcs.begin ()->first->getIden ();
+	
+}
+
+}
