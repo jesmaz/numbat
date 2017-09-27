a09f75022534a256169c2fe0d3051367434edfce
Jesse Mazis <recursiveowl@gmail.com>
2017-09-27 13:46:03 +1000

    Static operating now preserves numeric types

--- include/ast/passes/operate.hpp
+++ include/ast/passes/operate.hpp
@@ -102,7 +102,7 @@ NodePtr bitwiseBinary (const Numeric & type, const BasicArray <NodePtr> & args, 
 			}
 			break;
 	}
-	return std::make_shared <Number> (args [0]->getPos (), args [0]->getFile (), result);
+	return std::make_shared <Number> (args [0]->getPos (), args [0]->getFile (), result, Numeric::get (type.getArith (), type.getMinPrec ()));
 }
 
 template <typename FUNCTOR>
@@ -162,7 +162,7 @@ NodePtr standardBinary (const Numeric & type, const BasicArray <NodePtr> & args,
 			}
 			break;
 	}
-	return std::make_shared <Number> (args [0]->getPos (), args [1]->getFile (), result);
+	return std::make_shared <Number> (args [0]->getPos (), args [1]->getFile (), result, Numeric::get (type.getArith (), type.getMinPrec ()));
 }
 
 template <parser::OPERATION OPP>
