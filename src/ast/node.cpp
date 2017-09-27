61e8381027e6d5304ff43c847fe194f2d3a49a6e
Jesse Mazis <recursiveowl@gmail.com>
2017-09-27 18:30:37 +1000

    Added foldConstPass to the default set of transformations

--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -1,4 +1,5 @@
 #include <ast/node.hpp>
+#include <ast/passes/execute.hpp>
 #include <ast/passes/metatagswizzle.hpp>
 #include <ast/passes/reflectpass.hpp>
 #include <ast/passes/resolve.hpp>
@@ -11,6 +12,7 @@ NodePtr transform (const NodePtr & node) {
 	auto n = MetaTagSwizzlePass () (node);
 	n = ResolvePass () (n);
 	n = ReflectPass () (n);
+	n = FoldConstPass () (n);
 	return n;
 	
 }
