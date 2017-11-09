2c31e0fb2a2992de1a3805c0d464ad08fcf79f31
Jesse Mazis <recursiveowl@gmail.com>
2017-11-10 07:29:18 +1100

    When trasnforming the AST, every function will also be transformed

--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -1,3 +1,4 @@
+#include <ast/context.hpp>
 #include <ast/node.hpp>
 #include <ast/passes/execute.hpp>
 #include <ast/passes/metatagswizzle.hpp>
@@ -10,11 +11,43 @@ namespace AST {
 
 NodePtr transform (const NodePtr & node) {
 	
+	auto & funcs = Context::getAllFunctions ();
+	
 	auto n = MetaTagSwizzlePass () (node);
+	for (auto & f : funcs) {
+		if (f->getBody ()) {
+			f->replaceBody (MetaTagSwizzlePass () (f->getBody ()));
+		}
+	}
+	
 	n = ResolvePass () (n);
+	for (auto & f : funcs) {
+		if (f->getBody ()) {
+			f->replaceBody (ResolvePass () (f->getBody ()));
+		}
+	}
+	
 	n = ReflectPass () (n);
+	for (auto & f : funcs) {
+		if (f->getBody ()) {
+			f->replaceBody (ReflectPass () (f->getBody ()));
+		}
+	}
+	
 	n = FoldConstPass () (n);
+	for (auto & f : funcs) {
+		if (f->getBody ()) {
+			f->replaceBody (FoldConstPass () (f->getBody ()));
+		}
+	}
+	
 	n = PruneDeadCodePass () (n);
+	for (auto & f : funcs) {
+		if (f->getBody ()) {
+			f->replaceBody (PruneDeadCodePass () (f->getBody ()));
+		}
+	}
+	
 	return n;
 	
 }
