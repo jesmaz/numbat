9f7721152af008de63d94a343e8b0a0baff0969e
Jesse Mazis <recursiveowl@gmail.com>
2017-11-14 12:21:38 +1100

    Added dynamically growing stack for storing program state

---
+++ include/utility/literalstack.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include <utility/literals.hpp>
+#include <vector>
+
+
+struct LiteralStack {
+	
+	public:
+		
+		Literal & operator [] (size_t index) {
+			if (literals.size () < index) {
+				literals.resize (index + 1);
+			}
+			return literals [index];
+		}
+		
+		size_t reserve () {
+			literals.push_back (Literal ());
+			return literals.size () - 1;
+		}
+		
+	protected:
+	private:
+		
+		std::vector <Literal> literals;
+		
+};
