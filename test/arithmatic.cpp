8785020f70a86505d7f2680b64f24b5773b6de18
Jesse Mazis <recursiveowl@gmail.com>
2018-06-16 10:12:42 +1000

    Added subtraction test

--- test/arithmatic.cpp
+++ test/arithmatic.cpp
@@ -20,3 +20,13 @@ TEST_F (InterpreterTest, integer_addition) {
 	EXPECT_EQ (interpret ("1398231 + 3890278"), "5288509");
 	
 }
+
+TEST_F (InterpreterTest, integer_subtraction) {
+	
+	EXPECT_EQ (interpret ("1 - 1"), "0");
+	EXPECT_EQ (interpret ("2 - 1"), "1");
+	EXPECT_EQ (interpret ("2 - 0"), "2");
+	EXPECT_EQ (interpret ("0 - 2"), "-2");
+	EXPECT_EQ (interpret ("4 - 2 - 3"), "-1");
+	
+}
