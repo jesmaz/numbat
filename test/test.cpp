f1ed7fd9781ded13b151116ecc3ebdf92ca4db21
Jesse Mazis <recursiveowl@gmail.com>
2018-11-24 15:22:26 +1100

    Added some setup for testing

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,72 @@
+#include <ast/call.hpp>
+#include <ast/context.hpp>
+#include <ast/flowcontrol.hpp>
+#include <ast/function.hpp>
+#include <ast/meta.hpp>
+#include <ast/variable.hpp>
+#include <file.hpp>
 #include <gtest/gtest.h>
+#include <utility/config.hpp>
+
+
+BasicArray <string> executeFiles = {
+	"numbers/unsigned.nbt"
+};
+
 
 int main (int argl, char ** args) {
+	
+	{
+		
+		auto fPtr = std::make_shared <AST::Function> ();
+		auto context = std::make_unique <AST::Context> (*numbat::File::builtIn ()->getContext ().getRootContext (), fPtr.get ());
+		auto param = std::make_shared <AST::Variable> (
+			numbat::lexer::position {0, 0}, 
+			context->getSourceFile (), 
+			AST::Numeric::get (AST::Numeric::ArithmaticType::UINT, 1), 
+			0, 
+			AST::Variable::LOCATION::LOCAL, 
+			"b"
+		);
+		context->var ("b", param);
+		context->allocStack (param);
+		auto sysCall = std::make_shared <AST::SystemCall> (
+			numbat::lexer::position {0, 0}, 
+			numbat::File::builtIn (), 
+			AST::Numeric::get (AST::Numeric::ArithmaticType::INT, 32), 
+			"exit", 
+			BasicArray <AST::NodePtr> {
+				std::make_shared <AST::StaticValue> (
+					numbat::lexer::position {0, 0}, 
+					numbat::File::builtIn (), 
+					AST::Numeric::get (AST::Numeric::ArithmaticType::INT, 32), 
+					1
+				)
+			}
+		);
+		auto body = std::make_shared <AST::IfElse> (
+			numbat::lexer::position {0, 0},
+			numbat::File::builtIn (),
+			param,
+			param,
+			sysCall
+		);
+		
+		fPtr->params = {param->getType ()};
+		fPtr->replaceBody (body);
+		fPtr->positions ["b"] = 0;
+		fPtr->iden = "assert";
+		fPtr->initialStack = context->getLocalStack ();
+		
+		numbat::File::builtIn ()->getContext ().getRootContext ()->func ("assert", fPtr);
+		
+	}
+	
+	Literal l (mpq_class (4));
+	assert (l == l.to_double ());
+	
+	Config::mutableGlobalConfig ().const_folding = false;
+	Config::mutableGlobalConfig ().prune_dead_code = false;
 	::testing::InitGoogleTest (&argl, args);
 	return RUN_ALL_TESTS ();
 }
