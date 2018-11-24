eee7b2f3c0d80ef91ac49243edb4f64dd3539cf1
Jesse Mazis <recursiveowl@gmail.com>
2018-11-24 15:21:50 +1100

    Disabled inplace merge due to a data integrity issue

--- include/stackmachine/chunk.hpp
+++ include/stackmachine/chunk.hpp
@@ -2,6 +2,7 @@
 
 
 #include <algorithm>
+#include <iostream>
 #include <stackmachine/instruction.hpp>
 #include <utility/array.hpp>
 #include <utility/literals.hpp>
@@ -25,9 +26,10 @@ inline const Chunk operator + (const Chunk & lhs, const Chunk & rhs) {
 		lhs.data + rhs.data,
 		lhs.text + rhs.text
 	};
-	std::inplace_merge (c.layouts.begin (), c.layouts.begin () + lhs.layouts.size (), c.layouts.end ());
-	std::inplace_merge (c.functionLayouts.begin (), c.functionLayouts.begin () + lhs.functionLayouts.size (), c.functionLayouts.end ());
-	std::inplace_merge (c.data.begin (), c.data.begin () + lhs.data.size (), c.data.end ());
+	// inplace merge was setting numbers to 0 somehow, will look at replacing the tuple with a struct
+	//std::inplace_merge (c.layouts.begin (), c.layouts.begin () + lhs.layouts.size (), c.layouts.end ());
+	//std::inplace_merge (c.functionLayouts.begin (), c.functionLayouts.begin () + lhs.functionLayouts.size (), c.functionLayouts.end ());
+	//std::inplace_merge (c.data.begin (), c.data.begin () + lhs.data.size (), c.data.end ());
 	return c;
 }
 
@@ -36,9 +38,9 @@ inline const Chunk & operator += (Chunk & lhs, const Chunk & rhs) {
 	lhs.functionLayouts = lhs.functionLayouts + rhs.functionLayouts;
 	lhs.data = lhs.data + rhs.data;
 	lhs.text = lhs.text + rhs.text;
-	std::inplace_merge (lhs.layouts.begin (), lhs.layouts.end () - rhs.layouts.size (), lhs.layouts.end ());
-	std::inplace_merge (lhs.functionLayouts.begin (), lhs.functionLayouts.end () - rhs.functionLayouts.size (), lhs.functionLayouts.end ());
-	std::inplace_merge (lhs.data.begin (), lhs.data.end () - rhs.data.size (), lhs.data.end ());
+	//std::inplace_merge (lhs.layouts.begin (), lhs.layouts.end () - rhs.layouts.size (), lhs.layouts.end ());
+	//std::inplace_merge (lhs.functionLayouts.begin (), lhs.functionLayouts.end () - rhs.functionLayouts.size (), lhs.functionLayouts.end ());
+	//std::inplace_merge (lhs.data.begin (), lhs.data.end () - rhs.data.size (), lhs.data.end ());
 	return lhs;
 }
 
