d0045155a628481941224a4edcfcfaf1fd27eef3
Jesse Mazis <recursiveowl@gmail.com>
2018-12-10 23:15:43 +1100

    Implemented LT operator in the interpreter

--- include/stackmachine/interpreter.hpp
+++ include/stackmachine/interpreter.hpp
@@ -291,7 +291,12 @@ namespace stackmachine {
 		}
 		
 		void op_cmp_lt (const Instruction & inst, std::ostream & out) {
-			abort ();
+			auto rhs = popTop ();
+			auto lhs = popTop ();
+			Layout boolLayout (TYPE::u8);
+			boolLayout.literalToData (lhs < rhs, &(stack [stackPos]));
+			stackDataLayout [dataLayoutPos++] = boolLayout;
+			stackPos += boolLayout.getSize ();
 		}
 		
 		void op_cmp_lte (const Instruction & inst, std::ostream & out) {
